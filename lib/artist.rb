2f4693ae338606ea511d6318a46bddbbeacf3e08
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-16 16:51:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,22 +1,147 @@
-class Artist 
+class Artist
+  @@all = []
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(title, genre)
+    Song.new(title, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+
+end
+
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+
+  def artists
+    songs.map do |song|
+      song.artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name, artist, genre)
+    @name= name
+    @artist=artist
+    @genre=genre
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
+
+class Doctor
+  @@all=[]
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
+end
+
+
+class Patient
   attr_accessor :name 
   
   def initialize(name)
-    @name = name 
-  end 
+    @name=name
+  end
   
-  def self.all 
-    @@all 
-  end 
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
   
-  def songs 
-  end 
-  
-  def new_song
-  end 
-  
-  def genres 
-  end 
-  
-end 
+end
+
+class Appointment
+  attr_accessor :patient, :doctor, :date
+
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end

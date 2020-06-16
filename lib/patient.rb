90f93689244e68a013bd7bdc86f71b69ea65c4e4
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-16 16:52:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,22 +1,24 @@
 class Patient
-  
   attr_accessor :name 
   
   def initialize(name)
-    @name = name 
-  end 
+    @name=name
+  end
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
-  def new_appointment
-  end 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
   
-  def self.all 
-    @@all 
-  end 
-  
-  def appointments  
-  end 
-  
-  def doctors 
-  end 
-  
-end 
+end

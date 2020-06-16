519687305c9a63090746ca62645f030e2f3ba9cf
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-16 16:52:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,33 +29,7 @@ class Artist
 
 end
 
-class Genre
-  attr_accessor :name
 
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
-  end
-
-  def artists
-    songs.map do |song|
-      song.artist
-    end
-  end
-
-  def self.all
-    @@all
-  end
-
-end
 
 class Song 
   attr_accessor :name, :artist, :genre

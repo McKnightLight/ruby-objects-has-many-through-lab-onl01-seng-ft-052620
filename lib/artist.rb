9b9869cea65e49329eb31c85ea0f9ad55d65209f
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-16 16:51:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -128,20 +128,3 @@ class Patient
   
 end
 
-class Appointment
-  attr_accessor :patient, :doctor, :date
-
-  @@all = []
-
-  def initialize(date, patient, doctor)
-    @date = date
-    @patient = patient
-    @doctor = doctor
-
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-end
